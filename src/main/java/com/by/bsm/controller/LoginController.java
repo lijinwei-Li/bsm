e17d237aba2ffb90b0e0a580da3930da3ad38da5
lijw5 <lijw5@asiainfo.com>
2021-02-10 21:08:38 +0800

    添加初步的登录页面和功能

---
+++ src/main/java/com/by/bsm/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.by.bsm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 用于登录注册相关的控制器
+ * @author:Lijw
+ * @createTime: 2021-02-10 19:31
+ */
+@Controller
+@Slf4j
+public class LoginController {
+    @RequestMapping("/")
+    public String jumpLoginPage(){
+        return "login";
+    }
+    @PostMapping("/get")
+    @ResponseBody
+    public Object get(String userName,String pwd){
+        log.info("userName:{}-pwd:{}",userName,pwd);
+        Map<Object, Object> map = new HashMap<>();
+        map.put("code","200");
+        map.put("url","index.html");
+        return map;
+    }
+}
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,22 @@
+/**
+ * 请求登录接口
+ */
+function login() {
+    $.ajax({
+        //几个参数需要注意一下
+        type: "POST",//方法类型
+        dataType: "json",//预期服务器返回的数据类型
+        url: "/get" ,//url
+        data: $('#loginForm').serialize(),
+        success: function (result) {
+            console.log(result);//打印服务端返回的数据(调试用)
+            if (result.code == 200) {
+                //alert("SUCCESS");
+                window.location.replace(result.url);
+            };
+        },
+        error : function() {
+            alert("异常！");
+        }
+    });
+}
